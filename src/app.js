b4a93e2f90c7cf1dd2ec1264a6ac2ea76e16c4bb
priyanshu singh <priyanshusingh.89790@gmail.com>
2026-02-21 20:56:23 +0530

    add db

--- src/app.js
+++ src/app.js
@@ -16,10 +16,7 @@ dbConnection();
 app.use(cors({
   origin: ["http://localhost:5173"],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.use(cors());
 app.use(express.json())
 app.use(cookieParser())
 
