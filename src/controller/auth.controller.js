a8030a68f15bf49b315cd07e7ba05fcf8460ca00
priyanshu singh <priyanshusingh.89790@gmail.com>
2026-02-22 00:34:01 +0530

    fix login

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -56,7 +56,7 @@ exports.login = async (req, res) => {
       });
     }
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select("+password");
 
     if (!user) {
       return res.status(401).send({
