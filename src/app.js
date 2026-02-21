5244e43396d526abc402ab1b16c416c40379bc9a
priyanshu singh <priyanshusingh.89790@gmail.com>
2026-02-21 20:45:49 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.options("*", cors());
+app.use(cors());
 app.use(express.json())
 app.use(cookieParser())
 
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("✅ MongoDB Connected");
     } catch (error) {
         console.error("❌ MongoDB Connection Failed:", error.message);
